b3af1b1a6ef95c1b971e206f8b443715a33f07f3
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-09-15 11:22:31 -0400

    first commit

---
+++ Intro.js
@@ -0,0 +1,36 @@
+// using console log to check the file is working
+    //console.log( `Hello world!`);
+
+// comparing the differences between defined, undefined, null, & not defined
+   //var thing = 'some text'; // string
+   //var other = undeefined; // undefined
+   //var nothing = null; //null
+   //console.log(something); // not defined
+
+// Booleans examples
+    var something = true; 
+
+    if(something){
+        // do something
+        console.log('true');
+    } else {
+        // do something else
+        console.log('false');
+    }    
+
+// Primitive Data types:
+  // Definition: Primitive data types are not mutable, cannot be changed
+// Strings: a string is a sequence of characters used to to represent text surrounded by quotes ``,'',""
+// Numbers: hold numeric values, can be integers, decimals, negatives, positives, and so on..
+// Boolean: a logical data type that can have only the values true or false 
+// Null: represents a reference that points to a nonexistent value 
+// undefined: is a primitive value automatically assigned to variable that have just been declared
+
+// Non-Primitive Data Types:
+  // Definition: Non Prinitive data types are mutable. they can be changed
+// Objects: You define (and create) a JavaScript object with an object literal. color red,age
+// Arrays: An array is a special variable, which can hold more than one value at a time.
+// Functions: defined with the function keyword, followed by a name, followed by parentheses ().
+
+
+// CLI COMMANDS
