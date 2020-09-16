d37ea0307a72f237ff8a82ec4e2010fad03b3511
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-09-16 16:11:56 -0400

    added string exe

--- day3exercises copy.js
+++ day3exercises copy.js
@@ -27,3 +27,22 @@ var name = 'tony';
 var weight = '250 pounds';
 
 var sentence = `My animal is a ${ani}. His name is ${name} , and he weights ${weight}.`;
+// level 0
+    // Q0: Declare a string with the value euqal to your name
+    var nam = 'Samuel';
+
+    // Q1: Given the string, 'WE ARE SHOUTING!' log 'we are shouting'
+        // console.log('WE ARE SHOUTHING!'.toLowerCase());
+// level 1
+    // Q2: Declare a string and log its length 
+           var carnam = 'BMW'
+          // console.log(carnam.length);   
+    // Q3: Given the string 'Hello World!', log the 6th character.
+  console.log('Hello World'[5])
+  
+// level 2
+    // Q4: Given the string 'Welcome World?', replace the question mark with an exclamation
+
+
+// level 3
+    // given any string, log the first and last characters
