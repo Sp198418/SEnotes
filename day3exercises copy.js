4c2d42277cc2abb0dc60f829a0c24a6f3e614cbe
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-09-16 11:45:53 -0400

    Day2notes

---
+++ day3exercises copy.js
@@ -0,0 +1,29 @@
+// Define a new variable and set it equal to the string of your name
+    // Log this to the console
+    var x = 'Samuel';
+// Define a new variable and set it equal to a number 
+var z = 20;
+    // Log this to the console
+// Set a variable to a boolean value 
+
+var boolean = true;
+    // Log this to the console
+// 4. Create a variable called carName, assign the value Volvo to it.
+var carName = 'Volvo';
+
+// 5. Create a variable called num, assign the value 50 to it.
+var num = '50';
+// 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+var num1 = 5;
+var num2 = 10;
+// 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+var num3 = (num1 + num2);
+// 8. Create a set of variables that define an animal (age, height, name, etc...). Must be atleast 6 variables.
+
+var ani = 'Lion';
+var age = 8;
+var height = '5 foot';
+var name = 'tony';
+var weight = '250 pounds';
+
+var sentence = `My animal is a ${ani}. His name is ${name} , and he weights ${weight}.`;
