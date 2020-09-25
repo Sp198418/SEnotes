345a07d76856e440166bb5a6fa683cff6ebbf9e6
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-09-25 14:31:50 -0400

    codewars

--- Code_Wars.js
+++ Code_Wars.js
@@ -1,84 +1,52 @@
-//function even_or_odd(number) {
-           
-//     if (number % 2 === 0){
-      
-//      return "Even"
 
-//     }else {
- 
-//  return "Odd"
- 
-//  }
-    
-//  }
-
-//  console.log(even_or_odd(4))
-
-
-// You get an array of numbers, return the sum of all of the positives ones.
-
-// Example [1,-4,7,12] => 1 + 7 + 12 = 20
-
-// Note: if there is nothing to sum, the sum is default to 0.
-    
-//      let stats = [1,-4,7,12]
-
-// function positiveSum(arr) {
-//     let total = 0;
-
-//     for(let i = 0; i < arr.length; i++){
-//         const currentNum = arr[i];
-//         if(currentNum > 0){
-//           total = total+ currentNum;
-//         }
-//     }
-    
-
-//     return total;
-// }
-
+//   Write a function called repeat_str which repeats the given string src exactly count times
   
-// console.log(positiveSum(stats))
-
-// given a number, find its opposite.
-
-// function opposite(num) {
-//      return (-num)
-// }
-
-// console.log(opposite(8))
-
-
-// Your goal is to create a function that removes the first and last characters of a string. 
-// You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
-
-// let team = "Giants"
-// function removeChar(str){
-//     let teamsls = str.slice(0, -1)
-//     let finaloutput = teamsls.slice(1)
-//     return finaloutput
-// };
-//    console.log(removeChar(team))
-   
-// you are given a number and have to make it negative. But maybe the number is already negative?
-
-
-// function makeNegative(num) {
-           
-//     if (num < 0){
-
-//         return (num)
-
-//     } else {
-
-//         return (-num)
-
+// function repeatStr (n, s) {
+//     var output = '';
+//     for(var i=0; i<n; i++){
+//       output +=s;
 //     }
+//     return output;
+//   }
 
-// }
+
+// We need a function that can transform a number into a string.
+
+
+// function numberToString(num) {
     
-//      console.log(makeNegative(-42))
+//     return num.toString()
+//   }
+//   console.log(numberToString(55))
+
+// Complete the solution so that it reverses the string passed into it
+
+// function solution(str){
+//       let reverseString = '';
+//       for (var i = str.length-1; i >= 0; i--) {
+//         reverseString += str[i];
+//       }
+//       return reverseString;
+//     }
+//     console.log(solution('Apple'))
 
 
+// Given an array of integers your solution should find the smallest integer.
 
+// var args = [5, 6, 3, 34]
 
+// function findSmallestInt(args) {
+    
+//     var smallInt = args[0];
+    
+//     for(var i =0; i < args.length; i++){
+    
+//     if(args[i] <= smallInt){
+//     	smallInt = args[i];
+//     	}
+    
+//     } 
+//         return (smallInt)
+// }
+
+// console.log(findSmallestInt(args))
