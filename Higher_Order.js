5cc663826bb10e76dab7a181079588b1e75601be
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-09-29 00:47:32 -0400

    commented not/codewars

---
+++ Higher_Order.js
@@ -0,0 +1,67 @@
+// Higher order functions
+        // .filter()
+        // .reduce()      
+        // .map()
+  
+    
+
+// filter questions
+// Level 0
+    // given an array of numbers filter out any that are larger than 10
+    // let num = [8, 4, 6, 24,94]
+
+    // let largerNums = num.filter(function(n){return n > 10})
+    // console.log(largerNums)
+// Level 1
+    // given an array of numbers filter out any that are divisible by 3
+    //    let num = [2,1,18,30]
+    //    let dividedNums = num.filter(function(n){return n%3==0})
+    //     console.log(dividedNums)
+    // given an array of numbers filter out any that are even
+    //   let evennum = [2, 8, 1, 3]
+    //   let doubleNums = evennum.filter(function(n){return n%2==0})
+    // console.log(doubleNums)
+// Level 2
+    // given an array of strings, filter out any that are longer than 4 chars
+        //  let toys = ['Ball', 'Drone', 'Doll'] 
+        //  coolToys = toys.filter(function(n){
+        //      return n.length < 5
+        //  })
+        //  console.log(coolToys)
+
+
+    // given an array of arrays, filter out any that are shorter than 10 elements
+            //  let arr = [[video game, Soccer, basketball], [apple, banana, grape, orange,watermelon], , [1,2,3,4,5,6,7,8,9,10,11]]
+
+            //  Newarr = arr.filter(function(n){return n.length >= 10})
+            //  console.log(Newarr)
+
+// Level 3
+    // given an array of objects, filter out any that dont have a key called 'firstName'
+
+// reduce questions 
+// level 0:
+    // given an array of numbers add them all together
+
+// level 1:
+    // given an array of numbers, multiply them all together
+
+// level 2:
+    // given an array of strings, concatiante them together 
+
+// level 3: 
+    // given an array of arrays make them all into one array 
+
+// level 5:
+    // given an array of strings, remove any vowels and concatenate them toegher 
+
+
+// map questions
+// level 0:
+    // given an array of numbers, return a new array with all numbers multiplied by 2
+
+// level 1:
+    // given an array of numbers, return a new array with all numbers increased by 1
+
+// level 2:
+    // given an array of strings, return a new array with all strings as capitals
---
+++ code_Hw.js
@@ -0,0 +1,199 @@
+// Commented out describing solutions
+// level 1
+    // Q 1.1    
+        // Write some code that extracts the end of the email address
+        // Log that to the console 
+            // example 
+                // input:
+                // creating an object and giving it key values
+                // person = {
+                //     fullName = 'Nic Leask',
+                //     email = 'nic@gmail.com',
+                // }
+                // // output:
+                // '@gmail.com'
+                // setting the key value endOfEmail to the obj and using split to get the second part of the string
+                let endOfEmail = person.email.split('@')[1];
+                // console.log('@' + endOfEmail);
+
+// level 2
+    // Q 2.1
+        // write a conditional statement which checks if the person is 5'11", shorter, or taller.
+        // If the person is the same log, "Same height as Beckham"
+        // If the person is taller log, "Taller than Beckham"
+        // If the person is shorter log, "Shorter than Beckham"
+            // hint there are 12 inches in a foot, you may want to convert the height into only inches
+            // using a function with a param of str
+            function heightConverter(str){
+                // variable feet equal to foot value
+                let feet = Number(str.split("'")[0]);
+                // varialbe inches equal to the inches value using split
+                let inches = Number(str.split("'")[1].slice(0, -1));
+                // person.heightInInches is equal to converted height into to inches
+                person.heightInInches = inches + (feet*12);
+                console.log(person.heightInInches)
+            }
+
+            heightConverter(person.height);
+            console.log(person.heightInInches);
+        //   if statement generator outputs a string based on a <,> or === value 
+            if(person.heightInInches > 71){
+                console.log("Taller than Beckham");
+            } else if (person.heightInInches === 71) {
+                console.log("Same height as Beckham");
+            } else if (person.heightInInches < 71) {
+                console.log("Shorter than Beckham");
+            }
+
+    // Q 2.2
+        // Create a function which takes in an object 
+        // If the object doesn't have a specific key return 'No such value'.
+        // If it does, return that key's value. 
+        function hasValue(obj, str){
+            // if statement set object and key value to undefined
+            if(obj[str] !== undefined){
+                // the function will output the given string value
+                return `${person.fullName}, ${str}: ${obj[str]}`;
+            } else {
+                // if false the function will output (undefined) which is equal to the string undefined
+                return `${person.fullName}, ${str}: no such value.`;
+            }
+        }
+
+        console.log(hasValue(person, 'Nationality'))
+
+// level 3
+    // Q 3.1
+        // Using the full name property, get the person's initials.
+        // Create a new key called initials.
+        // Set the value of this key to the person's initials.
+            // Hint if you're getting stuck refer to last nights assignment in "SEInstructorNotes\Assignments\Day7Loops.js"
+
+        function initialCreator(obj){
+            // value set equal to the full split at each index
+            let arrOfNames = obj.fullName.split(' ');
+            // accumalator
+            obj.initials = '';
+            // for statement that runs arcoss the fullName
+            for(let i = 0; i < arrOfNames.length; i++){
+                // obj.initials equal to itself plus the first index
+                obj.initials = obj.initials + arrOfNames[i][0];
+            // } output the persons initials
+            return obj.initials;            
+        }
+
+        console.log(initialCreator(person));
+        //   function with param of person
+        function initial(person){
+            // variable nameSplt equal to string fullname split at each index
+            var nameSplt = person['fullName'].split(' ');
+            // accumlator
+            var initials2 = '';
+            // for statement that runs across the fullName string
+            for(var j = 0; j < nameSplt.length; j++){
+                //  takes the first letter out of the String
+              var name = nameSplt[j];
+              var initial = name[0];
+                // variable initial2 is equal itself plus initial
+              initials2 = initials2 + initial;
+            // } outputs the initial of the String value
+            return initials2;
+        }
+        console.log(initial(person));
+
+
+
+
+
+
+
+/// CODE WARS QUESTIONS
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// let arrayOfSheep = [
+//     true, true, true, false,
+//     true, true, true, true,
+//     true, false, true, false,
+//     true, false, false, true,
+//     true, true, true, true,
+//     false, false, true, true
+//   ];
+
+
+// function countSheeps(arrayOfSheep) {
+//     let sheepCounter = 0;
+    
+//       for( var i = 0; i <arrayOfSheep.length; i++) {
+       
+//         if(arrayOfSheep[i]=== true) {
+//           sheepCounter++;
+//         }
+//       }
+   
+//       return sheepCounter;
+//   }
+//    console.log(countSheeps(arrayOfSheep))
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// function numberSum(N) {
+//     var total = 0;
+//       for(var i = 1; i <= N; i++){
+//         total += i;
+//       }
+//       return total;
+//   }
+//   console.log(numberSum(3))
+
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// function squareSum(numbers){
+//     var sqNum = numbers.map(num=>num*num);
+//     var addNum = sqNum.reduce((acc,curr)=> acc + curr);
+//     console.log( addNum )
+//   };
+  
+//   console.log(squareSum([1, 2, 2]));
+
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// function basicOp(operation, value1, value2)
+// {
+//   if(operation == '+')return value1 + value2;
+//   if(operation == '-')return value1 - value2;
+//   if(operation == '*')return value1 * value2;
+//   if(operation == '/')return value1 / value2;
+// }
+
+// console.log(basicOp('*', 2,2))
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// function litres(time) {
+//     return Math.floor(time/2);
+//   }
+
+//   console.log(litres(2))
+
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.Given a year, return the century it is in.
+
+// function centuryFromYear(year) {
+
+//     return Math.ceil(year/100)
+
+// }
+
+// console.log(centuryFromYear(2020))
+
+
+
+
