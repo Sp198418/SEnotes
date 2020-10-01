f55e3816f4b98a397ad1869bcc4c7a61ec2597b8
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-10-01 16:04:50 -0400

    html and css + websites apple/supercars

--- 9_29_Codewars.js
+++ 9_29_Codewars.js
@@ -61,3 +61,27 @@
 //   }
 
 //   console.log(litres(3))
+
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// function vowelCount(str5)
+// {
+//   var vowelList = 'aeiou';
+//   var vcount = 0;
+  
+//   for(var i = 0; i < str5.length ; i++)
+//   {
+//     if (vowelList.indexOf(str5[i]) !== -1)
+//     {
+//       vcount += 1;
+//     }
+  
+//   }
+//   return vcount;
+// }
+// console.log(vowelCount("The waterfall view"));
