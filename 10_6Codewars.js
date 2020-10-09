93cbcd69bcf5d2ce439025b0864fad02724a743b
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-10-08 20:34:26 -0400

    New Cal

--- 10_6Codewars.js
+++ 10_6Codewars.js
@@ -58,3 +58,14 @@
 // }
 //     console.log(isIsogram('Mississippi'))
 //     console.log(isIsogram('Samuel'))
+
+
+
+// Write function avg which calculates average of numbers in given list.
+
+
+
+// function find_average(array) {
+//     return array.reduce((a, b) => (a + b)) / array.length;
+//   }
+// console.log(find_average([1,2,3,4]))
--- Css/CalculatorProject/Pro-cal.js
+++ Css/CalculatorProject/Pro-cal.js
@@ -6,35 +6,76 @@ var screen = document.querySelector('.display');
 console.log(screen.innerHTML)
 
 
+
+function addAllEventListeners(arr){
+
+  
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i].value === '=' || arr[i].value === 'C' ) {
+      arr[i].addEventListener('click', calculate);
+    }else {
+      arr[i].addEventListener('click', inputChange);
+    }
+
+  }
+}
+addAllEventListeners(allButtons);
+
 function inputChange(event){
   console.log(event.target.value)
   screen.innerHTML += event.target.value
 }
 
-function addAllEventListeners(arr){
-
+function calculate(event) {
+  // current clicked buttons value
+  var clickedButtonValue = event.target.value;
   
-    for(var i = 0; i < arr.length; i++){
+  if (clickedButtonValue === '=' && screen.innerHTML  !== '') {
+    // check if the display is not empty then only do the calculation
+      // calculate and show the answer to display
+       myEval(screen.innerHTML)
+     // screen.innerHTML  = eval(screen.innerHTML );
+      // if the C button is click and the display if not empty
+  }else if (clickedButtonValue === 'C' ) {
+       //display screen show cleared screen
+      screen.innerHTML = "";
+  }else if (clickedButtonValue == '+/-') {
+            myEval(screen.innerHTML)
+  }
+    
+  }  
   
-      arr[i].addEventListener('click', inputChange);
-    }
-  }
-  addAllEventListeners(allButtons);
-
-
- var str = '2 + 2'
-
- function operater(equals){
-  var arr = equals.split('')
-  var val1 = parseInt(arr[0])
-  var num = arr[1]
-  var val2 = parseInt(arr[2])
-
-  if(num === "+"){
-    return val1 + val2
-  }else if(num ==="-") {
-    return val1 - val2
-  }
- }
+function myEval(str){
+  // split on the operator and evaluate using the resulting array ['9', '+', '8']
    
+   var operator = str.match(/[^0-9 \.]/g)
+   console.log(operator, 'This is the opertor')
+  var nums = str.split(operator)
+  var firstNum = Number(nums[0]) 
+  var secondNum = Number(nums[1])
 
+  console.log(nums, 'nums',  operator, 'operator')
+  if(operator == '+'){
+    console.log(nums[0][1])
+    screen.innerHTML = firstNum + secondNum
+  }else if(operator == '-'){
+    console.log(nums[0][1])
+    screen.innerHTML = firstNum - secondNum
+  }else if(operator == '÷'){
+      console.log(nums[0][1])
+    screen.innerHTML = firstNum / secondNum 
+  }else if(operator == '×'){
+    console.log(nums[0][1])
+    screen.innerHTML = firstNum * secondNum 
+  }else if (operator == '+/-'){
+    console.log('We made it')
+
+  }
+}
+ 
+// function pos_to_neg(num){
+
+//   num > 0; num = -num
+// }  
+//   return Math.abs(num)
+// }
