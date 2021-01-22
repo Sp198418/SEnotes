ad11fb547ce382e208898e276877bd5ab9014c02
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2021-01-21 23:10:51 -0500

    sum of positive

--- JSEXERCISES/9_29_Codewars.js
+++ JSEXERCISES/9_29_Codewars.js
@@ -95,19 +95,36 @@
 
 // "I'd like to take this chance to apologize.. To absolutely NOBODY!"
 
-var quote = function(fighter) {
-    if (fighter === 'george saint pierre') {
-      return  "I am not impressed by your performance.";
-    } else if (fighter ===  'Conor McGregor') {
-    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
-    } else if (fighter === 'George Saint Pierre') {
-    return  "I am not impressed by your performance.";
-    } else if (fighter ===  'conor mcgregor'){
-   return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
-    } else if (fighter === 'GEORGE SAINT PIERRE') {
-    return  "I am not impressed by your performance.";
-    } else if (fighter ===  'CONOR MCGREGOR'){
-   return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
-    } 
-  };
+// var quote = function(fighter) {
+//     if (fighter === 'george saint pierre') {
+//       return  "I am not impressed by your performance.";
+//     } else if (fighter ===  'Conor McGregor') {
+//     return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+//     } else if (fighter === 'George Saint Pierre') {
+//     return  "I am not impressed by your performance.";
+//     } else if (fighter ===  'conor mcgregor'){
+//    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+//     } else if (fighter === 'GEORGE SAINT PIERRE') {
+//     return  "I am not impressed by your performance.";
+//     } else if (fighter ===  'CONOR MCGREGOR'){
+//    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+//     } 
+//   };
  
+// function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//    return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+// }
+
+// console.log(goals(1,2,3))
+
+function positiveSum(arr) {
+    let sum = 0
+    for(let i = 0; i < arr.length;i++){
+        if(arr[i] > 0){
+            sum += arr[i];
+        }
+    }
+    return sum ;
+}
+
+console.log(positiveSum([-1,2,3,4,-5]))
